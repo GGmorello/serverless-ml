3e6de99de8387eca6825ec1a85eed83d40e088ef
GiannisTheo <gt997@yahoo,gr>
2023-11-15 01:37:10 +0100

    get wine function

--- lab1/wine/wine-feature-pipeline-daily.py
+++ lab1/wine/wine-feature-pipeline-daily.py
@@ -29,22 +29,30 @@ def get_random_wine():
     """
     Returns a DataFrame containing one random iris flower
     """
-    virginica_df = generate_wine("Virginica", 8, 5.5, 3.8, 2.2, 7, 4.5, 2.5, 1.4)
-    versicolor_df = generate_wine("Versicolor", 7.5, 4.5, 3.5, 2.1, 3.1, 5.5, 1.8, 1.0)
-    setosa_df =  generate_wine("Setosa", 6, 4.5, 4.5, 2.3, 1.2, 2, 0.7, 0.3)
+    quality = random.uniform(3, 8)
+    wine = generate_wine(quality,fixed_acidity_range=(4.6, 15.9), volatile_acidity_range=(0.12, 1.58), citric_acid_range=(0,1),
+                         residual_sugar_range=(0.9, 15.5), chlorides_range=(0.012, 0.611), free_sulfur_dioxide_range=(1,72),
+                         total_sulfur_dioxide_range=(6,289), density_range=(0.99007,1.00369), ph_range=(2.74, 4.01), sulphates_range=(0.33, 2),
+                         alcohol_range=(8.4, 14.9))
+    return wine
+    # virginica_df = generate_wine("Virginica", 8, 5.5, 3.8, 2.2, 7, 4.5, 2.5, 1.4)
+    # versicolor_df = generate_wine("Versicolor", 7.5, 4.5, 3.5, 2.1, 3.1, 5.5, 1.8, 1.0)
+    # setosa_df =  generate_wine("Setosa", 6, 4.5, 4.5, 2.3, 1.2, 2, 0.7, 0.3)
 
-    pick_random = random.uniform(3, 8)
-    if pick_random >= 2:
-        iris_df = virginica_df
-        print("Virginica added")
-    elif pick_random >= 1:
-        iris_df = versicolor_df
-        print("Versicolor added")
-    else:
-        iris_df = setosa_df
-        print("Setosa added")
 
-    return iris_df
+
+    # pick_random = random.uniform(3, 8)
+    # if pick_random >= 2:
+    #     iris_df = virginica_df
+    #     print("Virginica added")
+    # elif pick_random >= 1:
+    #     iris_df = versicolor_df
+    #     print("Versicolor added")
+    # else:
+    #     iris_df = setosa_df
+    #     print("Setosa added")
+
+    # return iris_df
 
 def main():
     # Use github secrets to pass in the feature store password
